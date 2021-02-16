8aba75841c628dafafa0cab47e406b594e78b994
UgrinchukO <ugrinchuko@gmail.com>
2021-02-16 10:33:43 +0300

    added dependence

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -9,6 +9,13 @@ const Dialogs = (props) => {
     let dialogElements = props.state.dialogs.map(dialog => <DialogsItem name={dialog.name} id={dialog.id}/>)
     let messagesElements = props.state.messages.map(m => <MessageItem message={m.message}/>)
 
+    let itemArea = React.createRef()
+
+    let addPost = () => {
+        let text = itemArea.current.value
+        alert(text)
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
@@ -16,6 +23,10 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+            </div>
+            <div>
+                <textarea ref={itemArea}></textarea>
+                <button className={s.buttonItem} onClick={addPost}>Click!</button>
             </div>
         </div>
     )
