e8fa0503fa1069c2f9b73dc53b2083f6bf2ef171
UgrinchukO <ugrinchuko@gmail.com>
2021-02-25 16:48:55 +0300

    added func subscribe

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+    console.log("Save changed!")
+}
 
 let state = {
     dialogsPage: {
@@ -26,7 +28,7 @@ let state = {
     }
 }
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -38,10 +40,14 @@ export let addPost = () => {
     rerenderEntireTree(state)
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state)
 
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 export default state;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import {rerenderEntireTree} from "./render";
-import state from "./Redux/state";
 import reportWebVitals from "./reportWebVitals";
+import React, {} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addPost, subscribe, updateNewPostText} from './Redux/state'
+import {BrowserRouter} from "react-router-dom";
+import state from './Redux/state'
 
 
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(state)
+
+subscribe(rerenderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
 reportWebVitals();
-
